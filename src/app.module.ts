3148e65c42db6f953e687999c5b7a7e53ae9ed9c
gabriel80n <gabrielnpmoraes@hotmail.com>
2025-09-20 01:06:07 -0300

    fix: db

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,33 +11,11 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { PatientModule } from './patient/patient.module';
 import { ExamModule } from './exam/exam.module';
 import { RolesGuard } from './auth/guards/roles-auth.guard';
-import { Exam } from './database/entities/exam.entity';
-import { PasswordRecoveryCode } from './database/entities/password-recovery-code.entity';
-import { Patient } from './database/entities/patient.entity';
-import { User } from './user/entities/user.entity';
-
-import * as fs from 'fs';
-import * as path from 'path';
-
-const sslCertPath = path.resolve(__dirname, '../bundle/sa-east-1-bundle.pem');
+import ormConfig from './database/data_source';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432'),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [User, Patient, Exam, PasswordRecoveryCode],
-      synchronize: true,
-      autoLoadEntities: true,
-      ssl: {
-        ca: fs.readFileSync(sslCertPath).toString(),
-        rejectUnauthorized: true,
-      },
-    }),
+    TypeOrmModule.forRoot(ormConfig),
     UserModule,
     AuthModule,
     EmailModule,
--- src/database/data_source.ts
+++ src/database/data_source.ts
@@ -1,27 +1,42 @@
-// import { DataSource } from 'typeorm';
-// import { User } from './entities/user.entity';
-// import { Patient } from './entities/patient.entity';
-// import { Exam } from './entities/exam.entity';
-// import { PasswordRecoveryCode } from './entities/password-recovery-code.entity';
-// import * as fs from 'fs';
-// import * as path from 'path';
+import * as fs from 'fs';
+import { join } from 'path';
 
-// const sslCertPath = path.resolve(
-//   __dirname,
-//   '../../bundle/sa-east-1-bundle.pem',
-// );
+const rootPath = process.cwd();
 
-// export const AppDataSource = new DataSource({
-//   type: 'postgres',
-//   host: process.env.DB_HOST,
-//   port: parseInt(process.env.DB_PORT || '5432'),
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   entities: [User, Patient, Exam, PasswordRecoveryCode],
-//   synchronize: true, // apenas dev
-//   ssl: {
-//     ca: fs.readFileSync(sslCertPath).toString(),
-//     rejectUnauthorized: true,
-//   },
-// });
+const sslCertPath = join(rootPath, 'public', 'us-east-1-bundle.pem');
+
+const ormConfig = {
+  type: 'postgres' as const,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [join(rootPath, 'dist/**/*.entity{.ts,.js}')],
+  migrations: [join(rootPath, 'dist/migrations/*{.ts,.js}')],
+  synchronize: process.env.DB_SYNCHRONIZE === 'true',
+  logging: process.env.DB_LOGGING === 'true',
+  schema: process.env.DB_SCHEMA,
+  ssl: {
+    rejectUnauthorized: process.env.DB_REJECT_UNAUTHORIZED === 'true',
+    ca: fs.readFileSync(sslCertPath).toString(),
+  },
+  options: {
+    encrypt: process.env.DB_ENCRYPT === 'true',
+    trustServerCertificate: process.env.DB_TRUST_SERVER_CERTIFICATE === 'true',
+    integratedSecurity: process.env.DB_INTEGRATED_SECURITY === 'true',
+  },
+};
+
+// Ajusta as opções baseado nas variáveis de ambiente
+if (process.env.BD_HAS_OPTIONS !== 'true') {
+  delete ormConfig.options;
+} else if (!process.env.DB_ENCRYPT && ormConfig.options) {
+  delete ormConfig.options.encrypt;
+}
+
+if (process.env.DB_DOMAIN_CONTROLLER) {
+  (ormConfig as any).domain = process.env.DB_DOMAIN_CONTROLLER;
+}
+
+export default ormConfig;
