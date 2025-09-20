0be58f66787612e5492b2fe346b3f20c2e2df9e2
gabriel80n <gabrielnpmoraes@hotmail.com>
2025-09-20 00:49:47 -0300

    fix: database

--- src/database/data_source.ts
+++ src/database/data_source.ts
@@ -1,27 +1,27 @@
-import { DataSource } from 'typeorm';
-import { User } from './entities/user.entity';
-import { Patient } from './entities/patient.entity';
-import { Exam } from './entities/exam.entity';
-import { PasswordRecoveryCode } from './entities/password-recovery-code.entity';
-import * as fs from 'fs';
-import * as path from 'path';
+// import { DataSource } from 'typeorm';
+// import { User } from './entities/user.entity';
+// import { Patient } from './entities/patient.entity';
+// import { Exam } from './entities/exam.entity';
+// import { PasswordRecoveryCode } from './entities/password-recovery-code.entity';
+// import * as fs from 'fs';
+// import * as path from 'path';
 
-const sslCertPath = path.resolve(
-  __dirname,
-  '../../bundle/sa-east-1-bundle.pem',
-);
+// const sslCertPath = path.resolve(
+//   __dirname,
+//   '../../bundle/sa-east-1-bundle.pem',
+// );
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities: [User, Patient, Exam, PasswordRecoveryCode],
-  synchronize: true, // apenas dev
-  ssl: {
-    ca: fs.readFileSync(sslCertPath).toString(),
-    rejectUnauthorized: true,
-  },
-});
+// export const AppDataSource = new DataSource({
+//   type: 'postgres',
+//   host: process.env.DB_HOST,
+//   port: parseInt(process.env.DB_PORT || '5432'),
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   entities: [User, Patient, Exam, PasswordRecoveryCode],
+//   synchronize: true, // apenas dev
+//   ssl: {
+//     ca: fs.readFileSync(sslCertPath).toString(),
+//     rejectUnauthorized: true,
+//   },
+// });
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import 'dotenv/config';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import { AppDataSource } from './database/data_source';
 
 dotenv.config();
 
 export default async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Pipes
-  await AppDataSource.initialize();
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
